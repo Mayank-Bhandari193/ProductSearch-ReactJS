0cf6b0edf75836fec5873d989f11be750d5b007c
Mayank Bhandari <73786467+Mayank-Bhandari193@users.noreply.github.com>
2023-12-09 11:47:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import "./components/style.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+}
+
+export default App;
